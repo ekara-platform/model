582ac00f94e18a34b1edc47d566ae02701c75968
nirekin <nirekin@hotmail.com>
2019-02-11 11:04:11 +0100

    Add test for hook parameters inheritance

--- environment_test.go
+++ environment_test.go
@@ -353,6 +353,24 @@ func assertEnv(t *testing.T, env Environment) {
 	}
 
 	//------------------------------------------------------------
+	// Node2 Hook Env and Param
+	//------------------------------------------------------------
+
+	if assert.Equal(t, 31, len(no.Hooks.Provision.Before[0].parameters)) {
+		assert.Equal(t, "tasks_task2_param_key1_value_overwritten", no.Hooks.Provision.Before[0].parameters["tasks_task2_param_key1"])
+		assert.Equal(t, "tasks_task2_param_key2_value", no.Hooks.Provision.Before[0].parameters["tasks_task2_param_key2"])
+		assert.Equal(t, "tasks_task2_param_key3_value", no.Hooks.Provision.Before[0].parameters["tasks_task2_param_key3"])
+
+	}
+
+	if assert.Equal(t, 31, len(no.Hooks.Provision.Before[0].envVars)) {
+		assert.Equal(t, "tasks_task2_env_key1_value_overwritten", no.Hooks.Provision.Before[0].envVars["tasks_task2_env_key1"])
+		assert.Equal(t, "tasks_task2_env_key2_value", no.Hooks.Provision.Before[0].envVars["tasks_task2_env_key2"])
+		assert.Equal(t, "tasks_task2_env_key3_value", no.Hooks.Provision.Before[0].envVars["tasks_task2_env_key3"])
+
+	}
+
+	//------------------------------------------------------------
 	// Environment Stacks
 	//------------------------------------------------------------
 	stacks := env.Stacks
