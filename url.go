e09479ffaa16a2d0d5582eb426bbdda97e452e88
nirekingit <nirekin@hotmail.com>
2019-04-17 07:47:19 +0200

    Add url

---
+++ url.go
@@ -0,0 +1,202 @@
+package model
+
+import (
+	"fmt"
+	"io/ioutil"
+
+	"net/http"
+	"net/url"
+	"os"
+	pathIm "path"
+	"path/filepath"
+	"strings"
+)
+
+type (
+	EkUrl interface {
+		String() string
+		ReadUrl() ([]byte, error)
+		Scheme() string
+		SetScheme(s string)
+		Path() string
+		AsFilePath() string
+		Host() string
+		UpperScheme() string
+		SetDefaultScheme()
+		CheckSlashSuffix()
+		ResolveReference(repo string) (EkUrl, error)
+		AddPathSuffix(s string)
+		RemovePathSuffix(s string)
+	}
+
+	rootUrl struct {
+		url *url.URL
+	}
+
+	FileUrl struct {
+		*rootUrl
+		filePath string
+	}
+
+	RemoteUrl struct {
+		*rootUrl
+	}
+)
+
+/////////////////////////////////////////////
+
+func (ru *rootUrl) Path() string {
+	return ru.url.Path
+}
+
+func (ru *rootUrl) Host() string {
+	return ru.url.Host
+}
+
+func (ru *rootUrl) Scheme() string {
+	return ru.url.Scheme
+}
+
+func (ru *rootUrl) SetScheme(s string) {
+	ru.url.Scheme = s
+}
+
+func (ru *rootUrl) UpperScheme() string {
+	return strings.ToUpper(ru.url.Scheme)
+}
+
+func (ru *rootUrl) SetDefaultScheme() {
+	// If no protocol, assume file
+	if ru.Scheme() == SchemeUnknown {
+		ru.SetScheme(strings.ToLower(SchemeFile))
+	}
+}
+
+func (ru *rootUrl) CheckSlashSuffix() {
+	if !strings.HasSuffix(ru.Path(), "/") {
+		ru.AddPathSuffix("/")
+	}
+}
+
+func (ru *rootUrl) AddPathSuffix(s string) {
+	ru.url.Path = ru.url.Path + s
+}
+
+func (ru *rootUrl) RemovePathSuffix(s string) {
+	ru.url.Path = strings.TrimRight(ru.url.Path, s)
+}
+
+func (ru *rootUrl) String() string {
+	return ru.url.String()
+}
+
+func (ru RemoteUrl) ResolveReference(repo string) (EkUrl, error) {
+	repo = strings.TrimLeft(repo, "/")
+	repoU, e := url.Parse(repo)
+	if e != nil {
+		return RemoteUrl{}, e
+	}
+	return CreateUrl(ru.url.ResolveReference(repoU).String())
+}
+
+func (ru FileUrl) ResolveReference(repo string) (EkUrl, error) {
+	repo = strings.TrimLeft(repo, "/")
+	repoU := ru.filePath + filepath.ToSlash(repo)
+	return CreateUrl(repoU)
+}
+
+/////////////////////////////////////////////
+func createFileUrl(path string) (EkUrl, error) {
+	absPath, e := filepath.Abs(path)
+	if e != nil {
+		return nil, e
+	}
+	if DirExist(absPath) {
+		absPath = absPath + string(filepath.Separator)
+	}
+	r := FileUrl{filePath: absPath}
+
+	absPath = filepath.ToSlash(absPath)
+	absPath = strings.TrimLeft(absPath, "/")
+	absPath = "file:///" + absPath
+	u, e := url.Parse(absPath)
+	if e != nil {
+		return r, e
+	}
+	u.Path = pathIm.Clean(u.Path)
+	r.rootUrl = &rootUrl{url: u}
+	r.CheckSlashSuffix()
+	return r, nil
+}
+
+func createRemoteUlr(path string) (EkUrl, error) {
+	r := RemoteUrl{}
+	u, e := url.Parse(path)
+	if e != nil {
+		return r, e
+	}
+	r.rootUrl = &rootUrl{url: u}
+	r.CheckSlashSuffix()
+	return r, nil
+}
+
+func CreateUrl(path string) (EkUrl, error) {
+	var r EkUrl
+	var e error
+	// If file exists locally, resolve its absolute path and convert it to an URL
+
+	if b, _ := FileExist(path); b {
+		r, e = createFileUrl(path)
+		if e != nil {
+			return r, e
+		}
+	} else {
+		r, e = createRemoteUlr(path)
+		if e != nil {
+			return r, e
+		}
+	}
+	r.SetDefaultScheme()
+	return r, nil
+}
+
+func (ru FileUrl) ReadUrl() ([]byte, error) {
+	location := ru.filePath
+
+	file, err := os.Open(location)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	content, err := ioutil.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+	return content, nil
+}
+
+func (ru FileUrl) AsFilePath() string {
+	return ru.filePath
+}
+
+func (ru RemoteUrl) ReadUrl() ([]byte, error) {
+	var response *http.Response
+	response, err := http.Get(ru.url.String())
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+	if response.StatusCode != 200 {
+		err = fmt.Errorf("error reading URL %s, HTTP status %d", ru.url.String(), response.StatusCode)
+		return nil, err
+	}
+	content, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+	return content, nil
+}
+
+func (ru RemoteUrl) AsFilePath() string {
+	return ""
+}
---
+++ url_test.go
@@ -0,0 +1,215 @@
+package model
+
+import (
+	"net/url"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPath(t *testing.T) {
+	u, e := url.Parse("http://www.google.com/my_path")
+	assert.Nil(t, e)
+	ru := rootUrl{url: u}
+	assert.Equal(t, u.Path, ru.Path())
+}
+
+func TestScheme(t *testing.T) {
+	u, e := url.Parse("http://www.google.com/my_path")
+	assert.Nil(t, e)
+	ru := rootUrl{url: u}
+	assert.Equal(t, u.Scheme, ru.Scheme())
+	assert.Equal(t, strings.ToUpper(SchemeHttp), ru.UpperScheme())
+	assert.Equal(t, SchemeHttp, ru.UpperScheme())
+}
+
+func TestSetScheme(t *testing.T) {
+	u, e := url.Parse("http://www.google.com/my_path")
+	assert.Nil(t, e)
+	ru := rootUrl{url: u}
+	assert.Equal(t, SchemeHttp, ru.UpperScheme())
+	ru.SetScheme(SchemeHttps)
+	assert.Equal(t, SchemeHttps, ru.UpperScheme())
+}
+
+func TestDefaultScheme(t *testing.T) {
+	u, e := url.Parse("//www.google.com/my_path")
+	assert.Nil(t, e)
+	ru := rootUrl{url: u}
+	assert.Equal(t, SchemeUnknown, ru.Scheme())
+	ru.SetDefaultScheme()
+	assert.Equal(t, SchemeFile, ru.UpperScheme())
+}
+
+func TestCheckSlashSuffixHttpOk(t *testing.T) {
+	u, e := url.Parse("http://www.google.com/my_path")
+	assert.Nil(t, e)
+	ru := rootUrl{url: u}
+	ru.CheckSlashSuffix()
+	assert.True(t, hasSuffixIgnoringCase(ru.Path(), "/my_path/"))
+}
+
+func TestCheckSlashSuffixHttpKo(t *testing.T) {
+	u, e := url.Parse("http://www.google.com/my_path/")
+	assert.Nil(t, e)
+	ru := rootUrl{url: u}
+	ru.CheckSlashSuffix()
+	assert.True(t, hasSuffixIgnoringCase(ru.Path(), "/my_path/"))
+}
+
+func TestAddSuffix(t *testing.T) {
+	u, e := url.Parse("http://www.google.com/my_path/")
+	assert.Nil(t, e)
+	ru := rootUrl{url: u}
+	ru.AddPathSuffix("dummySuffix")
+	assert.True(t, hasSuffixIgnoringCase(ru.Path(), "/my_path/dummySuffix"))
+}
+
+func TestRemoveSuffix(t *testing.T) {
+	u, e := url.Parse("http://www.google.com/my_path/")
+	assert.Nil(t, e)
+	ru := rootUrl{url: u}
+	ru.RemovePathSuffix("/")
+	assert.True(t, hasSuffixIgnoringCase(ru.Path(), "/my_path"))
+}
+
+func TestCreateRemoteUlr(t *testing.T) {
+	p := "http://www.google.com/my_path"
+	u, e := createRemoteUlr(p)
+	assert.Nil(t, e)
+	assert.Equal(t, SchemeHttp, u.UpperScheme())
+	assert.True(t, hasSuffixIgnoringCase(u.String(), "/"))
+	assert.Equal(t, "/my_path/", u.Path())
+
+	val, ok := u.(RemoteUrl)
+	assert.True(t, ok)
+	// The slash suffix is almays added
+	assert.Equal(t, val.rootUrl.String(), p+"/")
+}
+
+func TestCreateFileUlr(t *testing.T) {
+
+	var p string
+	wd, e := os.Getwd()
+	assert.Nil(t, e)
+
+	if os.PathSeparator == '/' {
+		p = wd + "/some/base"
+	} else if os.PathSeparator == '\\' {
+		p = wd + "\\some\\base"
+	}
+
+	u, e := createFileUrl(p)
+	assert.Nil(t, e)
+
+	os.Remove(p)
+	assert.Nil(t, e)
+
+	assert.Equal(t, SchemeFile, u.UpperScheme())
+	assert.True(t, hasSuffixIgnoringCase(u.String(), "/"))
+	val, ok := u.(FileUrl)
+	assert.True(t, ok)
+	if os.PathSeparator == '/' {
+		assert.Equal(t, p, val.filePath)
+	} else if os.PathSeparator == '\\' {
+		assert.Equal(t, p, val.filePath)
+	}
+
+	ps := filepath.ToSlash(p)
+
+	assert.Equal(t, val.rootUrl.String(), "file:///"+ps+"/")
+
+}
+
+func TestCreateBasedRemoteUrl(t *testing.T) {
+
+	p1 := "http://github.com"
+	p2 := "organisation/repository"
+
+	env := &yamlEnvironment{
+		Ekara: yamlEkara{Base: p1},
+	}
+	b, e := CreateComponentBase(env)
+	assert.Nil(t, e)
+	u, e := b.CreateBasedUrl(p2)
+	assert.Nil(t, e)
+	assert.Equal(t, SchemeHttp, u.UpperScheme())
+
+	val, ok := u.(RemoteUrl)
+	assert.True(t, ok)
+	assert.Equal(t, val.rootUrl.String(), p1+"/"+p2+"/")
+}
+
+func TestCreateBasedRemoteUrl2(t *testing.T) {
+
+	p1 := "http://github.com/"
+	p2 := "/organisation/repository/"
+
+	env := &yamlEnvironment{
+		Ekara: yamlEkara{Base: p1},
+	}
+	b, e := CreateComponentBase(env)
+	assert.Nil(t, e)
+	u, e := b.CreateBasedUrl(p2)
+	assert.Nil(t, e)
+	assert.Equal(t, SchemeHttp, u.UpperScheme())
+
+	val, ok := u.(RemoteUrl)
+	assert.True(t, ok)
+	assert.Equal(t, val.rootUrl.String(), p1+p2[1:])
+}
+
+func TestCreateBasedLocalUrl(t *testing.T) {
+
+	var p1 string
+	var p2 string
+	wd, e := os.Getwd()
+	assert.Nil(t, e)
+
+	if os.PathSeparator == '/' {
+		p1 = wd + "/some/base/"
+		p2 = "organisation/repository"
+	} else if os.PathSeparator == '\\' {
+		p1 = wd + "\\some\\base\\"
+		p2 = "organisation\\repository"
+	}
+
+	e = os.MkdirAll(p1+p2, 0777)
+	assert.Nil(t, e)
+	assert.True(t, DirExist(p1+p2))
+
+	env := &yamlEnvironment{
+		Ekara: yamlEkara{Base: p1},
+	}
+
+	b, e := CreateComponentBase(env)
+	assert.Nil(t, e)
+	assert.Equal(t, SchemeFile, b.Url.UpperScheme())
+	u, e := b.CreateBasedUrl(p2)
+	assert.Nil(t, e)
+	assert.Equal(t, SchemeFile, u.UpperScheme())
+
+	val, ok := u.(FileUrl)
+	assert.True(t, ok)
+
+	if os.PathSeparator == '/' {
+		assert.Equal(t, p1+p2+"/", val.filePath)
+	} else if os.PathSeparator == '\\' {
+		assert.Equal(t, p1+p2+"\\", val.filePath)
+	}
+
+	ps := filepath.ToSlash(p1 + p2 + "/")
+	assert.Equal(t, val.rootUrl.String(), "file:///"+ps)
+	defer func(wd string) {
+		var e error
+		if os.PathSeparator == '/' {
+			e = os.RemoveAll(wd + "/some/")
+		} else if os.PathSeparator == '\\' {
+			e = os.RemoveAll(wd + "\\some\\")
+		}
+		assert.Nil(t, e)
+	}(wd)
+}
