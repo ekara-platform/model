6e973a38e79e8089d38fe389446f5666a51ae0db
nirekingit <nirekin@hotmail.com>
2019-04-17 07:43:59 +0200

    Add url scheme

---
+++ scheme.go
@@ -0,0 +1,16 @@
+package model
+
+const (
+	//SchemeFile  scheme for a file
+	SchemeFile string = "FILE"
+	//SchemeGit  scheme for Git
+	SchemeGits string = "GIT"
+	//SchemeSvn  scheme for svn
+	SchemeSvn string = "SVN"
+	//SchemeHttp  scheme for http
+	SchemeHttp string = "HTTP"
+	//SchemeHttps  scheme for https
+	SchemeHttps string = "HTTPS"
+	//SchemeUnknown scheme is unknown
+	SchemeUnknown string = ""
+)
