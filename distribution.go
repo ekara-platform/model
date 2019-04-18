161125a44949396993e56836b0bdd18258a5a426
nirekingit <nirekin@hotmail.com>
2019-04-18 11:55:48 +0200

    Add distribution

---
+++ distribution.go
@@ -0,0 +1,44 @@
+package model
+
+import (
+	"errors"
+)
+
+const (
+
+	//EkaraDistribution The default repository for the ekara distribution
+	ekaraDistribution = "ekara-platform/distribution"
+)
+
+type Distribution Component
+
+//CreateDistribution creates the distribution
+//	Parameters
+//
+//		base: the base URL where to look for the distribution
+//		yamlEnv: the descriptor defining the distribution
+func CreateDistribution(base Base, yamlEnv *yamlEnvironment) (Distribution, error) {
+	defaulted := base.Defaulted()
+	repo := yamlEnv.Ekara.Distribution.Repository
+	if defaulted {
+		//When the base is the dault one we always use the default distribution
+		repo = ekaraDistribution
+	} else {
+		if repo == "" {
+			//If the distribution is not specified we must look for the default one
+			// into the default base even if the project has defined its own base
+			base, _ = CreateBase("")
+			repo = ekaraDistribution
+			defaulted = true
+		}
+	}
+	repoDist, e := CreateRepository(base, repo, yamlEnv.Ekara.Distribution.Ref, "")
+	if e != nil {
+		return Distribution{}, errors.New("invalid distribution repository: " + e.Error())
+	}
+	if !defaulted {
+		repoDist.setAuthentication(yamlEnv.Ekara.Distribution)
+	}
+	c := CreateComponent(EkaraComponentId, repoDist)
+	return Distribution(c), nil
+}
