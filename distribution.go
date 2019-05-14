83be12356b33624c13c73a4f86249106a880df62
nirekin <nirekin@hotmail.com>
2019-05-14 11:00:43 +0200

    Change rules for default distribution

--- distribution.go
+++ distribution.go
@@ -19,27 +19,28 @@ type Distribution Component
 //		base: the base URL where to look for the distribution
 //		yamlEnv: the descriptor defining the distribution
 func CreateDistribution(base Base, yamlEnv *yamlEnvironment) (Distribution, error) {
-	defaulted := base.Defaulted()
+	//defaulted := base.Defaulted()
 	repo := yamlEnv.Ekara.Distribution.Repository
-	if defaulted {
-		//When the base is the dault one we always use the default distribution
+	//if defaulted {
+	//	//When the base is the dault one we always use the default distribution
+	//	log.Printf("-->GBE the distribution is defaulted on base")
+	//	repo = ekaraDistribution
+	//} else {
+	if repo == "" {
+		//If the distribution is not specified we must look for the default one
+		// into the default base even if the project has defined its own base
+		//base, _ = CreateBase("")
 		repo = ekaraDistribution
-	} else {
-		if repo == "" {
-			//If the distribution is not specified we must look for the default one
-			// into the default base even if the project has defined its own base
-			base, _ = CreateBase("")
-			repo = ekaraDistribution
-			defaulted = true
-		}
+		//defaulted = true
 	}
+	//}
 	repoDist, e := CreateRepository(base, repo, yamlEnv.Ekara.Distribution.Ref, "")
 	if e != nil {
 		return Distribution{}, errors.New("invalid distribution repository: " + e.Error())
 	}
-	if !defaulted {
-		repoDist.setAuthentication(yamlEnv.Ekara.Distribution)
-	}
+	//if !defaulted {
+	repoDist.setAuthentication(yamlEnv.Ekara.Distribution)
+	//}
 	c := CreateComponent(EkaraComponentId, repoDist)
 	return Distribution(c), nil
 }
