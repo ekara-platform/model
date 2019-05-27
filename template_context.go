c50893f13eabb98088d5570cd391ac2a03090666
nirekin <nirekin@hotmail.com>
2019-05-27 16:07:53 +0200

    Add template context

---
+++ template_context.go
@@ -0,0 +1,28 @@
+package model
+
+type (
+	//TemplateContext the context passed to all ekara templates
+	TemplateContext struct {
+		//Vars represents all variable passed into the context,
+		// Thoses variables are the ones commit from the CLI
+		// parameters file merged with the ones comming from
+		// each environment descriptor.
+		Vars Parameters
+	}
+)
+
+//CreateContext Returns a template context
+func CreateContext(params Parameters) *TemplateContext {
+	return &TemplateContext{
+		Vars: params,
+	}
+}
+
+func (cc *TemplateContext) mergeVars(others Parameters) error {
+	var err error
+	cc.Vars, err = cc.Vars.inherit(others)
+	if err != nil {
+		return err
+	}
+	return nil
+}
---
+++ template_context_test.go
@@ -0,0 +1,59 @@
+package model
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateTemplateContext(t *testing.T) {
+	p, err := createParameters(map[string]interface{}{
+		"key1": "value1",
+		"key2": "value2",
+	})
+	assert.Nil(t, err)
+	assert.Equal(t, 2, len(p))
+
+	pc := CreateContext(p)
+
+	assert.NotNil(t, pc)
+	assert.Equal(t, 2, len(pc.Vars))
+	va, ok := pc.Vars["key1"]
+	assert.True(t, ok)
+	assert.Equal(t, va, "value1")
+
+	va, ok = pc.Vars["key2"]
+	assert.True(t, ok)
+	assert.Equal(t, va, "value2")
+}
+
+func TestMergeTemplateContext(t *testing.T) {
+	p, _ := createParameters(map[string]interface{}{
+		"key1": "value1",
+		"key2": "value2_owverwritten",
+	})
+
+	others, _ := createParameters(map[string]interface{}{
+		"key2": "value2",
+		"key3": "value3",
+	})
+
+	pc := CreateContext(p)
+	err := pc.mergeVars(others)
+	assert.Nil(t, err)
+	assert.NotNil(t, pc)
+	assert.Equal(t, 3, len(pc.Vars))
+	va, ok := pc.Vars["key1"]
+	assert.True(t, ok)
+	assert.Equal(t, va, "value1")
+
+	// Value should be overwritten
+	va, ok = pc.Vars["key2"]
+	assert.True(t, ok)
+	assert.Equal(t, va, "value2_owverwritten")
+
+	// Missing stuff should be added
+	va, ok = pc.Vars["key3"]
+	assert.True(t, ok)
+	assert.Equal(t, va, "value3")
+}
